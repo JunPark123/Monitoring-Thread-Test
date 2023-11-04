690515e7e075fefb65106829a9630565796a69a7
jpark <jpark@hubis.co.kr>
2023-11-04 23:43:21 +0900

    초기 커밋.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(380, 212);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(355, 35);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(768, 189);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 74);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "상태";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 24;
+            this.listBox1.Location = new System.Drawing.Point(69, 106);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(287, 340);
+            this.listBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1095, 732);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Thread _thread;
+        public Thread _checkthread;
+
+        public delegate void EHandle();
+        public EHandle monitoringValue;
+
+        private int m_value;
+        private Recv recv;
+
+        private bool isEventSuccess = false; // 이벤트 성공 여부 플래그
+        public Form1()
+        {
+            InitializeComponent();
+            m_value = 0;
+           
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            recv = new Recv();
+            recv.MyEvent += EventHandler; // 이벤트 핸들러 등록                                         
+            _thread = new Thread(recvThread); // 별도의 스레드에서 recv.Start() 실행
+            //_checkthread = new Thread(new ThreadStart(checkThread)); //checkthread 선언
+            monitoringValue = new EHandle(OnSomeEvent);
+
+            _thread.Start();
+            //_checkthread.Start();
+        }
+
+        public void recvThread()
+        {
+            while (true)
+            {
+                recv.start();
+                Thread.Sleep(500); // 1초 대기
+            }
+        }
+
+        public void checkThread()
+        {
+            while(true)
+            {
+                this.Invoke(monitoringValue);
+                Thread.Sleep(500);
+            }
+        }
+
+        private void EventHandler(string message)
+        {
+            //여기서 메세지를 전달받는다
+            //전달받은 메세지를 리스트박스에 계속 추가한다.
+            // UI 스레드에서 리스트 박스에 추가
+            if (listBox1.InvokeRequired)
+            {
+                listBox1.Invoke(new Action(() => 
+                {
+                    OnSomeEvent();
+                    listBox1.Items.Add(message);
+                    listBox1.SelectedIndex = listBox1.Items.Count - 1;
+                    
+                }));               
+                m_value = int.Parse(message.Substring(5));               
+            }
+            else
+            {
+                listBox1.Items.Add(message);
+            }
+
+           
+
+        }
+
+        private void OnSomeEvent()
+        {
+            if (m_value == 33)
+            {
+                isEventSuccess = true;
+                button1.BackColor = System.Drawing.Color.Green;
+            }
+            else
+            {
+                isEventSuccess = true;
+                button1.BackColor = System.Drawing.Color.Red;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if(_thread!=null)
+            {
+                _thread.Abort();
+                _thread.Join();
+
+                _checkthread.Abort();
+                _checkthread.Join();
+            }
+        }
+    }
+
+    public class EventClass
+    {
+        public string message = "";
+        private EventHandler handler;
+        public event EventHandler someEvent
+        {
+            add
+            {
+
+                handler += value;
+            }
+            remove
+            {
+
+                handler -= value;
+            }
+        }
+
+        public void doEvent()
+        {
+            handler?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    public class Recv
+    {
+        public delegate void RecvEvent(string message);
+
+        public event RecvEvent MyEvent;
+
+        public void start()
+        {
+            int a = creatRandomVal();
+            MyEvent?.Invoke("값은 : " + a.ToString());
+            // 일정 시간 동안 대기 혹은 다른 조건을 검사하여 스레드를 중단할 수 있음
+        }
+
+        public int creatRandomVal()
+        {
+            Random random = new Random();
+            int ran = 0;
+            ran = random.Next(30,40);
+            return ran;
+        }
+    }
+}
